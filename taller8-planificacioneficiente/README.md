bf787d4589419728dc630f4dc507618f594490cf
David Hernan Guevara Guevara <dhguevara@gmail.com>
2026-07-21 12:09:08 -0500

    Se agrego la clase database para el manejo a la base de datos

---
+++ taller8-planificacioneficiente/README.md
@@ -0,0 +1,5 @@
+# Taller inicial del proyecto
+## Modulo: Planificador Académico Eficiente
+### Elementos:
+Clase docente
+Clase database
--- taller8-planificacioneficiente/main.py
+++ taller8-planificacioneficiente/main.py
@@ -1,3 +1,6 @@
 from docente import docente
+from database import database
 
+database_pe = database()
 docente_1 = docente("Juan Pérez", "Las Aguas", "0911111111", "jperez@uide.edu.ec")
+database_pe.cerrar()
